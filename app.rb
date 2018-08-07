2e575da65ecc13b026117ee01915c82040028b0c
Ricardo Eihara <ricardo.hejr@gmail.com>
2018-08-07 14:08:00 -0300

    Add init project

---
+++ app.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+#
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('Gemfile', __dir__)
+
+require 'sinatra'
+require 'sinatra/contrib'
+require 'logger'
+require 'byebug'
+
+set :logger, Logger.new(STDOUT)
+
+route :get, :post, :delete, :put, :patch, '/*/?' do
+  content_type 'application/json'
+
+  logger.info "#{request.request_method} method - params: #{params}"
+  logger.info "#{request.body.read}"
+  {
+    body: request.body.read,
+    query_string: request.query_string,
+    headers: headers,
+    params: params,
+    request_method: request.request_method
+  }.to_json
+end
